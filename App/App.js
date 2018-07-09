27c9f7ac5fad04249c2d1ecff48856db5c4bc6b8
Matthieu Napoli <m.napoli@thecodingmachine.com>
2018-07-09 14:50:12 +0200

    Add a directory layout for presentational and container components
    It is recommended to [separate presentational and container components](https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0). Both kind of components will now have their own directory.
    
    The content of `App.js` has been moved to a container in order to show an example of a container component.

---
+++ App/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import {HomeScreen} from "./Containers/HomeScreen";
+
+export default class App extends Component {
+  render() {
+    return (
+      <HomeScreen/>
+    )
+  }
+}
---
+++ App/Containers/README.md
@@ -0,0 +1,1 @@
+This directory contains [container components](https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0), i.e. React components responsible for the logic of the application.
