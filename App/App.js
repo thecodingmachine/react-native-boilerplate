fbfdafc28a64d10cfaa5493b09a91f792b6dbfd5
Matthieu Napoli <m.napoli@thecodingmachine.com>
2018-07-10 09:48:26 +0200

    Fix coding style

--- App/App.js
+++ App/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
-import {HomeScreen} from "App/Containers/HomeScreen";
+import { HomeScreen } from 'App/Containers/HomeScreen'
 
 export default class App extends Component {
   render() {
-    return (
-      <HomeScreen/>
-    )
+    return <HomeScreen />
   }
 }
--- App/Containers/HomeScreen.js
+++ App/Containers/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Platform, StyleSheet, Text, View} from 'react-native'
+import { Platform, StyleSheet, Text, View } from 'react-native'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
