cd487588cee388153ec68103ea0482df22010595
woi <w.obry@thecodingmachine.com>
2019-01-30 18:04:51 +0100

    update store to fetch user data

---
+++ App/Services/UserService.js
@@ -0,0 +1,44 @@
+import { create } from 'apisauce'
+import { Config } from 'App/Config'
+
+/**
+ * This is an example of a service that connects to a 3rd party API.
+ *
+ * Feel free to remove this example from your application.
+ */
+
+const userApiClient = create({
+  /**
+   * Import the config from the App/Config/index.js file
+   */
+  baseURL: Config.API_URL,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  timeout: 3000,
+})
+
+function fetchUser() {
+  // Simulate an error 50% of the time just for testing purposes
+  // if (Math.random() > 0.5) {
+  //   return new Promise(function(resolve, reject) {
+  //     resolve(null)
+  //   })
+  // }
+  let number = Math.floor(Math.random() / 0.1) + 1
+  console.log(number)
+
+  return userApiClient.get(number.toString()).then((response) => {
+    console.log(response.data)
+    if (response.ok) {
+      return response.data
+    }
+
+    return null
+  })
+}
+
+export const userService = {
+  fetchUser,
+}
--- App/Stores/Example/InitialState.js
+++ App/Stores/Example/InitialState.js
@@ -4,7 +4,7 @@ import { Map } from 'immutable'
  * The initial values for the redux state.
  */
 export const INITIAL_STATE = Map({
-  temperature: null,
-  temperatureErrorMessage: null,
-  temperatureIsLoading: false,
+  user: null,
+  userIsLoading: false,
+  userErrorMessage: null,
 })
