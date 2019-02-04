df7007149cb7053393781e1eafede1b447d409d7
woi <w.obry@thecodingmachine.com>
2019-02-04 18:34:48 +0100

    update all dependencies out reduxsauce

--- App/Sagas/index.js
+++ App/Sagas/index.js
@@ -1,11 +1,12 @@
-import { takeLatest } from 'redux-saga/effects'
+import { takeLatest, all } from 'redux-saga/effects'
 import { ExampleTypes } from 'App/Stores/Example/Actions'
 import { StartupTypes } from 'App/Stores/Startup/Actions'
 import { fetchTemperature } from './ExampleSaga'
 import { startup } from './StartupSaga'
 
 export default function* root() {
-  yield [
+  console.log('index.js saga')
+  yield all([
     /**
      * @see https://redux-saga.js.org/docs/basics/UsingSagaHelpers.html
      */
@@ -13,5 +14,5 @@ export default function* root() {
     takeLatest(StartupTypes.STARTUP, startup),
     // Call `fetchTemperature()` when a `FETCH_TEMPERATURE` action is triggered
     takeLatest(ExampleTypes.FETCH_TEMPERATURE, fetchTemperature),
-  ]
+  ])
 }
