dbea2883088dd7a57ddf0e813f94907c5b7464d7
aum <a.mutin@thecodingmachine.com>
2019-04-01 16:03:28 +0200

    Full upgrade to RN 0.59.2

--- android/app/src/main/java/com/boilerplate/MainApplication.java
+++ android/app/src/main/java/com/boilerplate/MainApplication.java
@@ -3,7 +3,6 @@ package com.boilerplate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,8 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNGestureHandlerPackage()
+          new MainReactPackage()
       );
     }
 
