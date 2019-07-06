9a9851e6eb5d823c816a8c796f3fa6f718240e0e
unknown <thomaszhou974@gmail.com>
2019-07-06 12:43:08 +0200

    added assets folder and helpers.js

---
+++ App/Theme/Helpers.js
@@ -0,0 +1,150 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  fill: {
+    flex: 1,
+  },
+  center: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  fillCenter: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  rowReverse: {
+    flexDirection: 'row-reverse',
+  },
+  column: {
+    flexDirection: 'column',
+  },
+  columnReverse: {
+    flexDirection: 'column-reverse',
+  },
+  mainStart: {
+    justifyContent: 'flex-start',
+  },
+  mainCenter: {
+    justifyContent: 'center',
+  },
+  mainEnd: {
+    justifyContent: 'flex-end',
+  },
+  mainSpaceBetween: {
+    justifyContent: 'space-between',
+  },
+  mainSpaceAround: {
+    justifyContent: 'space-around',
+  },
+  crossStart: {
+    alignItems: 'flex-start',
+  },
+  crossCenter: {
+    alignItems: 'center',
+  },
+  crossEnd: {
+    alignItems: 'flex-end',
+  },
+  crossStretch: {
+    alignItems: 'stretch',
+  },
+  selfStretch: {
+    alignSelf: 'stretch',
+  },
+  rowMain: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  rowCross: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  rowCenter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  colMain: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  colCross: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  colCenter: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  fillRow: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  fillRowReverse: {
+    flex: 1,
+    flexDirection: 'row-reverse',
+  },
+  fillRowMain: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  fillRowCross: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  fillRowCenter: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  fillCol: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  fillColReverse: {
+    flex: 1,
+    flexDirection: 'column-reverse',
+  },
+  fillColMain: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  fillColCross: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  fillColCenter: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textCenter: {
+    textAlign: 'center',
+  },
+  textJustify: {
+    textAlign: 'justify',
+  },
+  textLeft: {
+    textAlign: 'left',
+  },
+  textRight: {
+    textAlign: 'right',
+  },
+  backgroundReset: {
+    backgroundColor: 'transparent',
+  },
+  fullWidth: {
+    width: '100%',
+  },
+})
