6196b5c8fca098ea022b4c70d6d109564a768e58
unknown <thomaszhou974@gmail.com>
2019-07-06 12:44:15 +0200

    updated colors

--- App/Theme/Colors.js
+++ App/Theme/Colors.js
@@ -7,8 +7,12 @@
 
 export default {
   transparent: 'rgba(0,0,0,0)',
-  // Example colors:
+  white: '#FFF',
+  darkWhite: '#FCFCFC',
   text: '#212529',
+  border: '#979797',
+  main: '#54B256',
+  header: '#2ED276',
   primary: '#007bff',
   success: '#28a745',
   error: '#dc3545',
