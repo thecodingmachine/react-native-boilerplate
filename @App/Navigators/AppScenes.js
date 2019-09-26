e0dc5e74220dbe300e1bb7342e626e5f1e6ca24c
Kuyen <iamcxa@gmail.com>
2019-09-26 11:59:18 +0800

    unite to arrow function

--- @App/Navigators/AppScenes.js
+++ @App/Navigators/AppScenes.js
@@ -5,11 +5,11 @@ import { Stack, Scene } from 'react-native-router-flux';
 import RootScreen from 'App/Containers/Root/RootScreen';
 import ExampleScreen from 'App/Containers/Example/ExampleScreen';
 
-export default function() {
+export default () => {
   return (
     <Stack key="root">
       <Scene key="RootScreen" component={RootScreen} initial />
       <Scene key="ExampleScreen" component={ExampleScreen} />
     </Stack>
   );
-}
+};
