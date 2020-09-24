245d69ee6c703e30b73e2518bd9a55d29e5d83da
JED <jeremy.dolle34@gmail.com>
2020-09-24 10:33:17 +0200

    resolve import

--- template/src/Store/Startup/Actions.js
+++ template/src/Store/Startup/Actions.js
@@ -1,4 +1,4 @@
-import { initApplication as init } from '@/Services'
+import { initApplication as init } from '@/Services/Startup'
 import { createAsyncThunk } from '@reduxjs/toolkit'
 
 export const initApplication = createAsyncThunk('app/initApplication', init)
