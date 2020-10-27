ed457e50ab6a51734db7b5b98caec93b9283dbc5
Arln <aurelien.mutin@gmail.com>
2020-10-27 17:06:34 +0100

    Setup post-init script and upgrade configuration usage
    * executable post init script
    * post init script with instructions and copy configuration file
    * change instruction according to the new post init script

--- post-init.script.js
+++ post-init.script.js
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
 
-console.log("This is post init script");
+const fs = require('fs')
+fs.copyFile('src/Config/index.example.js', 'src/Config/index.js', (err) => {
+    if (err) throw err;
+    console.log('src/Config/index.example.js was copied to src/Config/index.js');
+});
+
+console.log("TheCodingMachine React-Native Boilerplate initialized with success !");
+console.log("" +
+    "                                                                    .-`    `::  \n" +
+    " `/////////////     `/shhhy+-   ://.        /sy/                   /ss/   :NMN: \n" +
+    " `sssssyhhhyhhy:` `yMMMMNNMMMd. osss:     `hMMMh-    .-`         `+ss:   +MMm/. \n" +
+    "      :MMM+----. `mMMm+:-`.oo/` osssyo`  `dMMMMh-   ohhh-       `oyy+-  sMMd/.  \n" +
+    "      :MMM+`     oMMN/-         osssyhs`.mMMMMMh-   -hds`      .syy+- `hMMh:`   \n" +
+    "      :MMM+`     hMMd:          oss-ohhhMMMhdMMh-    `.       +NNm/. .dMMy:`    \n" +
+    "      :MMM+`     oMMN/          oss-`shdMMd:dMMh-    :-`     sMMd/. -NMMo-      \n" +
+    "      :MMM+`     `mMMm/`  `/+:. oss- `sdMd:.hMMh-   hMys-  `hMMh:` -hdy-`       \n" +
+    "      :MMM+`      `yMMMMdssss+  oss.  `oh/- hMMh-   -hyo:  dMMs:` -ss+`         \n" +
+    "      .oys:`        .+yhyo/-    -::`    ``  -syo-     ..`  +mo-   `//           \n" +
+    "        ``             ```                    ``            `.                  ")
+
+console.log('- If you need to read more about this boilerplate : https://thecodingmachine.github.io/react-native-boilerplate/')
+console.log('- If you have some troubles : https://github.com/thecodingmachine/react-native-boilerplate/issues')
+console.log('- If you love this boilerplate, give us a star, you will be a ray of sunshine in our lives :) https://github.com/thecodingmachine/react-native-boilerplate')
--- template.config.js
+++ template.config.js
@@ -9,6 +9,7 @@ module.exports = {
     // Directory with the template which will be copied and processed by React Native CLI. Template directory should have package.json with all dependencies specified, including `react-native`.
     templateDir: './template',
 
-    // Path to script, which will be executed after initialization process, but before installing all the dependencies specified in the template. This script runs as a shell script but you can change that (e.g. to Node) by using a shebang (see example custom template).
+    // Path to script, which will be executed after initialization process, but before installing all the dependencies specified in the template.
+    // This script runs as a shell script but you can change that (e.g. to Node) by using a shebang (see example custom template).
     postInitScript: './post-init.script.js',
 };
