ebc8a8cda82ba931f711d3382d180c28bf782335
JED <jeremy.dolle34@gmail.com>
2020-11-04 15:04:07 +0100

    rename initial state var

--- template/src/Store/User/index.js
+++ template/src/Store/User/index.js
@@ -2,8 +2,8 @@ import { buildSlice } from '@thecodingmachine/redux-toolkit-wrapper'
 import FetchOne from './FetchOne'
 
 // This state is common to all the "user" module, and can be modified by any "user" reducers
-const moduleInitialState = {
+const sliceInitialState = {
   item: {},
 }
 
-export default buildSlice('user', [FetchOne], moduleInitialState).reducer
+export default buildSlice('user', [FetchOne], sliceInitialState).reducer
