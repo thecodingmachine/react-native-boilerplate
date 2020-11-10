2c71f50a960fa0ba5a2fd075639990282432609b
JED <jeremy.dolle34@gmail.com>
2020-11-10 18:49:36 +0100

    jest add mock and update async storage dependency

---
+++ template/__tests__/setup.js
@@ -0,0 +1,4 @@
+import 'react-native-gesture-handler/jestSetup.js'
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock'
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage)
--- template/src/Store/index.js
+++ template/src/Store/index.js
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-community/async-storage'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { combineReducers } from 'redux'
 import {
   persistReducer,
