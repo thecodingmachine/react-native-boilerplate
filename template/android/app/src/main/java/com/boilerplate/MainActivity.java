130007520036ae0029ca0deff30beee747401592
Angelk90 <angelo.k90@hotmail.it>
2020-12-08 12:21:41 +0100

    Fix Android for changing theme at runtime

--- template/android/app/src/main/java/com/boilerplate/MainActivity.java
+++ template/android/app/src/main/java/com/boilerplate/MainActivity.java
@@ -1,6 +1,7 @@
 package com.boilerplate;
 
 import com.facebook.react.ReactActivity;
+import android.content.res.Configuration;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Boilerplate";
+  }
+  
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    getReactInstanceManager().onConfigurationChanged(this, newConfig);
   }
 }
