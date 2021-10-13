5c9bd17e7ed2e2d5e5a0346df7e14bf5f75a9a5f
William <w.obry@thecodingmachine.com>
2021-10-13 14:51:12 +0200

    merge Example container

--- template/src/Containers/ExampleContainer.js
+++ template/src/Containers/ExampleContainer.js
@@ -36,109 +36,106 @@ const ExampleContainer = () => {
   const onChangeTheme = ({ theme, darkMode }) => {
     dispatch(changeTheme({ theme, darkMode }))
 
+    const getLanguage = () => i18n.language
 
-  const getLanguage = () => i18n.language
+    const setLanguage = async language => {
+      await i18n.changeLanguage(language)
+      setInternationalLanguage(!internationalLanguage)
+    }
 
-  const setLanguage = async (language) => {
-    await i18n.changeLanguage(language)
-    setInternationalLanguage(!internationalLanguage)
-  }
+    const toggleLanguage = () => {
+      getLanguage() === 'fr' ? setLanguage('en') : setLanguage('fr')
+    }
 
-  const toggleLanguage = () => {
-    getLanguage() === 'fr' ? setLanguage('en') : setLanguage('fr')
-  }
-
-  return (
-    <ScrollView
-      style={Layout.fill}
-      contentContainerStyle={[
-        Layout.fill,
-        Gutters.smallHPadding,
-      ]}
-    >
-      <View
-        style={[
-          Layout.rowReverse,
-          Gutters.smallTMargin,
-          Gutters.smallRMargin,
-          Gutters.largeBMargin,
-        ]}
+    return (
+      <ScrollView
+        style={Layout.fill}
+        contentContainerStyle={[Layout.fill, Gutters.smallHPadding]}
       >
-        <View style={[Layout.row, Layout.center]}>
-          <Text style={[Fonts.textRegular, Gutters.tinyHPadding]}>FR</Text>
-          <Switch
-            thumbColor={Colors.primary}
-            onValueChange={toggleLanguage}
-            value={internationalLanguage}
-            trackColor={{
-              false: Colors.inputBackground,
-              true: Colors.trackTrueColor,
-            }}
-          />
-          <Text style={[Fonts.textRegular, Gutters.tinyHPadding]}>EN</Text>
+        <View
+          style={[
+            Layout.rowReverse,
+            Gutters.smallTMargin,
+            Gutters.smallRMargin,
+            Gutters.largeBMargin,
+          ]}
+        >
+          <View style={[Layout.row, Layout.center]}>
+            <Text style={[Fonts.textRegular, Gutters.tinyHPadding]}>FR</Text>
+            <Switch
+              thumbColor={Colors.primary}
+              onValueChange={toggleLanguage}
+              value={internationalLanguage}
+              trackColor={{
+                false: Colors.inputBackground,
+                true: Colors.trackTrueColor,
+              }}
+            />
+            <Text style={[Fonts.textRegular, Gutters.tinyHPadding]}>EN</Text>
+          </View>
         </View>
-      </View>
-      <View style={[[Layout.colCenter, Gutters.smallHPadding]]}>
-        <Brand />
-        {(isLoading || isFetching) && <ActivityIndicator />}
-        {!isSuccess ? (
-          <Text style={Fonts.textRegular}>{error}</Text>
-        ) : (
-          <Text style={Fonts.textRegular}>
-            {t('example.helloUser', { name: data?.name })}
+        <View style={[[Layout.colCenter, Gutters.smallHPadding]]}>
+          <Brand />
+          {(isLoading || isFetching) && <ActivityIndicator />}
+          {!isSuccess ? (
+            <Text style={Fonts.textRegular}>{error}</Text>
+          ) : (
+            <Text style={Fonts.textRegular}>
+              {t('example.helloUser', { name: data?.name })}
+            </Text>
+          )}
+        </View>
+        <View
+          style={[
+            Layout.row,
+            Layout.rowHCenter,
+            Gutters.smallHPadding,
+            Gutters.largeVMargin,
+            Common.backgroundPrimary,
+          ]}
+        >
+          <Text style={[Layout.fill, Fonts.textCenter, Fonts.textSmall]}>
+            {t('example.labels.userId')}
           </Text>
-        )}
-      </View>
-      <View
-        style={[
-          Layout.row,
-          Layout.rowHCenter,
-          Gutters.smallHPadding,
-          Gutters.largeVMargin,
-          Common.backgroundPrimary,
-        ]}
-      >
-        <Text style={[Layout.fill, Fonts.textCenter, Fonts.textSmall]}>
-          {t('example.labels.userId')}
-        </Text>
-        <TextInput
-          onChangeText={setUserId}
-          editable={!isLoading}
-          keyboardType={'number-pad'}
-          maxLength={1}
-          value={userId}
-          selectTextOnFocus
-          style={[Layout.fill, Common.textInput]}
-        />
-      </View>
+          <TextInput
+            onChangeText={setUserId}
+            editable={!isLoading}
+            keyboardType={'number-pad'}
+            maxLength={1}
+            value={userId}
+            selectTextOnFocus
+            style={[Layout.fill, Common.textInput]}
+          />
+        </View>
 
-      <View style={[Layout.colCenter]}>
-        <Text style={[Fonts.textRegular, Gutters.smallBMargin]}>
-          {t('example.labels.darkMode')}
-        </Text>
-      <TouchableOpacity
-        style={[Common.button.rounded, Gutters.regularBMargin]}
-        onPress={() => onChangeTheme({ darkMode: null })}
-      >
-        <Text style={Fonts.textRegular}>{t('example.labels.auto')}</Text>
-      </TouchableOpacity>
+        <View style={[Layout.colCenter]}>
+          <Text style={[Fonts.textRegular, Gutters.smallBMargin]}>
+            {t('example.labels.darkMode')}
+          </Text>
+          <TouchableOpacity
+            style={[Common.button.rounded, Gutters.regularBMargin]}
+            onPress={() => onChangeTheme({ darkMode: null })}
+          >
+            <Text style={Fonts.textRegular}>{t('example.labels.auto')}</Text>
+          </TouchableOpacity>
 
-      <TouchableOpacity
-        style={[Common.button.outlineRounded, Gutters.regularBMargin]}
-        onPress={() => onChangeTheme({ darkMode: true })}
-      >
-        <Text style={Fonts.textRegular}>{t('example.labels.dark')}</Text>
-      </TouchableOpacity>
+          <TouchableOpacity
+            style={[Common.button.outlineRounded, Gutters.regularBMargin]}
+            onPress={() => onChangeTheme({ darkMode: true })}
+          >
+            <Text style={Fonts.textRegular}>{t('example.labels.dark')}</Text>
+          </TouchableOpacity>
 
-      <TouchableOpacity
-        style={[Common.button.outline, Gutters.regularBMargin]}
-        onPress={() => onChangeTheme({ darkMode: false })}
-      >
-        <Text style={Fonts.textRegular}>{t('example.labels.light')}</Text>
-      </TouchableOpacity>
-      </View>
-    </ScrollView>
-  )
+          <TouchableOpacity
+            style={[Common.button.outline, Gutters.regularBMargin]}
+            onPress={() => onChangeTheme({ darkMode: false })}
+          >
+            <Text style={Fonts.textRegular}>{t('example.labels.light')}</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    )
+  }
 }
 
 export default ExampleContainer
