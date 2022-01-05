790f4ff322aea071893730b087b2a4eee833e5f6
JED <jeremy.dolle34@gmail.com>
2022-01-05 09:48:15 +0100

    dependencies update

--- template/android/app/src/main/java/com/boilerplate/MainApplication.java
+++ template/android/app/src/main/java/com/boilerplate/MainApplication.java
@@ -1,5 +1,7 @@
 package com.boilerplate;
 
+import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -32,6 +34,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ReanimatedJSIModulePackage(); // <- add
         }
       };
 
