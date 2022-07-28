064f83c7800311b78d02abefc9593f6076738db4
DOLLE <jeremy.dolle34@gmail.com>
2022-07-28 14:04:53 +0200

    (RN version): bump rn version to 0.69.3 (#306)

--- template/android/app/src/main/java/com/boilerplate/MainApplication.java
+++ template/android/app/src/main/java/com/boilerplate/MainApplication.java
@@ -7,7 +7,9 @@ import android.content.Context;
 import com.facebook.react.bridge.JSIModulePackage;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.react.ReactInstanceManager;
+import com.boilerplate.newarchitecture.MainApplicationReactNativeHost;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -44,14 +46,23 @@ public class MainApplication extends Application implements ReactApplication {
         }
       };
 
+  private final ReactNativeHost mNewArchitectureNativeHost =
+      new MainApplicationReactNativeHost(this);
+
   @Override
   public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      return mNewArchitectureNativeHost;
+    } else {
+      return mReactNativeHost;
+    }
   }
 
   @Override
   public void onCreate() {
     super.onCreate();
+    // If you opted-in for the New Architecture, we enable the TurboModule system
+    ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
