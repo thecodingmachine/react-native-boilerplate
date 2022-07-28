b3e1230b371f71572f96cbb78a26041b0f748aef
JED <jeremy.dolle34@gmail.com>
2022-07-28 17:59:51 +0200

    (CI): WIP

--- template/src/App.js
+++ template/src/App.js
@@ -6,7 +6,7 @@ import { store, persistor } from '@/Store'
 import ApplicationNavigator from '@/Navigators/Application'
 import './Translations'
 
-console.log('TO REMOVE: CI TEST 4')
+console.log('TO REMOVE: CI TEST 6')
 
 const App = () => (
   <Provider store={store}>
