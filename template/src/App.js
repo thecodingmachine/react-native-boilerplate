af1bc1d6118262592f63fe98c1368dd5030b406b
JED <jeremy.dolle34@gmail.com>
2022-07-28 13:38:00 +0200

    (App): remove doc

--- template/src/App.js
+++ template/src/App.js
@@ -8,13 +8,6 @@ import './Translations'
 
 const App = () => (
   <Provider store={store}>
-    {/**
-     * PersistGate delays the rendering of the app's UI until the persisted state has been retrieved
-     * and saved to redux.
-     * The `loading` prop can be `null` or any react instance to show during loading (e.g. a splash screen),
-     * for example `loading={<SplashScreen />}`.
-     * @see https://github.com/rt2zz/redux-persist/blob/master/docs/PersistGate.md
-     */}
     <PersistGate loading={null} persistor={persistor}>
       <ApplicationNavigator />
     </PersistGate>
