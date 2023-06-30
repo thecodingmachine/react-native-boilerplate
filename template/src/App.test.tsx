a9a0b80ae9f50881239b2495eed0a24b6414c7c0
jeremydolle <j.dolle@thecodingmachine.com>
2023-06-30 13:38:05 +0200

    feat(rn): update react-native to 0.72

--- template/src/App.test.tsx
+++ template/src/App.test.tsx
@@ -2,6 +2,9 @@ import 'react-native';
 import React from 'react';
 import App from './App';
 
+// Note: import explicitly to use the types shiped with jest.
+import { it } from '@jest/globals';
+
 // Note: test renderer must be required after react-native.
 import { Provider } from 'react-redux';
 import { store } from './store';
