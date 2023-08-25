380a3425deddba462ae171bd86a370fb2a1e9d5f
DOLLE <jeremy.dolle34@gmail.com>
2023-08-25 12:34:30 +0200

    fix(android): rework the splashscreen implementation (#377)
    Co-authored-by: jeremydolle <j.dolle@thecodingmachine.com>

--- template/android/app/src/main/java/com/boilerplate/MainActivity.java
+++ template/android/app/src/main/java/com/boilerplate/MainActivity.java
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -28,5 +29,11 @@ public class MainActivity extends ReactActivity {
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstance) {
+    super.onCreate(savedInstance);
+    setTheme(R.style.AppTheme);
   }
 }
