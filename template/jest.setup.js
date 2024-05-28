4fb9a183c75ac8f710d2dc64e2a9b5091a13b5ab
jeremydolle <j.dolle@thecodingmachine.com>
2024-05-28 13:16:53 +0200

    chore(version): bump template deps and react native to 0.74

--- template/jest.setup.js
+++ template/jest.setup.js
@@ -1,6 +1,7 @@
 // import 'whatwg-fetch';
 import 'react-native-gesture-handler/jestSetup';
 import '@testing-library/jest-native/extend-expect';
+import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
 
 jest.mock('react-native-reanimated', () =>
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
@@ -9,3 +10,5 @@ jest.mock('react-native-reanimated', () =>
 
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
