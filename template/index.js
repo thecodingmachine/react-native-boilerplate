d0e81fb92c124400830595855f60d29227f55c47
jeremydolle <j.dolle@thecodingmachine.com>
2024-05-28 13:17:31 +0200

    feat(debug): add reactotron debugger

--- template/index.js
+++ template/index.js
@@ -6,4 +6,8 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
+if (__DEV__) {
+	import('@/reactotron.config');
+}
+
 AppRegistry.registerComponent(appName, () => App);
