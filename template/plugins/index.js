3d13d7ad1624fddd1a24f47895a4eb5700e8423e
jeremydolle <j.dolle@thecodingmachine.com>
2024-10-08 15:36:12 +0200

    chore: Refactor prompts.js and index.js
    Refactor the prompts.js and index.js files in the template/plugins/compile-js directory.
    The changes include fixing indentation and formatting issues, as well as updating variable names and adding comments for clarity.

--- template/plugins/index.js
+++ template/plugins/index.js
@@ -20,25 +20,25 @@ const plugins = [typescriptPlugin, printSuccessPlugin];
  * @return {Promise<*>}
  */
 async function applyPlugin(name, plugin, response) {
-	if (!plugin.promptsOptions) {
-		await plugin.apply(null, response);
-		return { [name]: null, ...response };
-	}
-	const { value } = await prompts({ ...plugin.promptsOptions, name: 'value' });
-	await plugin.apply(value, response);
-	return { [name]: value, ...response };
+  if (!plugin.promptsOptions) {
+    await plugin.apply(null, response);
+    return { [name]: null, ...response };
+  }
+  const { value } = await prompts({ ...plugin.promptsOptions, name: 'value' });
+  await plugin.apply(value, response);
+  return { [name]: value, ...response };
 }
 
 module.exports = {
-	/**
-	 * Apply all plugin in the order of plugin
-	 * @return {Promise<{apply: function(*=): Promise<void>, name: string, promptsOptions: {onRender(*): void, initial: boolean, name: string, type: string, message: string}|{type?: string, name?: string, message?: string, msg?: *, initial?: boolean, onRender?: function(*): void}}|{name?: string, promptsOptions?: {onRender(*): void, initial: boolean, name: string, type: string, message: string}|{type?: string, name?: string, message?: string, msg?: *, initial?: boolean, onRender?: function(*): void}, apply?: function(*=): Promise<void>}|{apply: function(*, *): Promise<Promise.Promise|Promise<unknown>>, name: string, promptsOptions: null}|{name?: string, promptsOptions?: null, apply?: function(*, *): Promise<Promise.Promise|Promise<unknown>>}>}
-	 */
-	async applyPlugins() {
-		return plugins.reduce(
-			(acc, { name, ...plugin }) =>
-				acc.then(response => applyPlugin(name, plugin, response)),
-			Promise.resolve({})
-		);
-	},
+  /**
+   * Apply all plugin in the order of plugin
+   * @return {Promise<{apply: function(*=): Promise<void>, name: string, promptsOptions: {onRender(*): void, initial: boolean, name: string, type: string, message: string}|{type?: string, name?: string, message?: string, msg?: *, initial?: boolean, onRender?: function(*): void}}|{name?: string, promptsOptions?: {onRender(*): void, initial: boolean, name: string, type: string, message: string}|{type?: string, name?: string, message?: string, msg?: *, initial?: boolean, onRender?: function(*): void}, apply?: function(*=): Promise<void>}|{apply: function(*, *): Promise<Promise.Promise|Promise<unknown>>, name: string, promptsOptions: null}|{name?: string, promptsOptions?: null, apply?: function(*, *): Promise<Promise.Promise|Promise<unknown>>}>}
+   */
+  async applyPlugins() {
+    return plugins.reduce(
+      (acc, { name, ...plugin }) =>
+        acc.then((response) => applyPlugin(name, plugin, response)),
+      Promise.resolve({}),
+    );
+  },
 };
