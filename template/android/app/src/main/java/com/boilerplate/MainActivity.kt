0385b02d387a890c4a1f59dfbf27ee1db639e644
jeremydolle <j.dolle@thecodingmachine.com>
2026-02-25 15:49:13 +0100

    chore: update build tools and SDK versions in Gradle files

--- template/android/app/src/main/java/com/boilerplate/MainActivity.kt
+++ template/android/app/src/main/java/com/boilerplate/MainActivity.kt
@@ -5,6 +5,7 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory;
 
 class MainActivity : ReactActivity() {
 
@@ -23,6 +24,7 @@ class MainActivity : ReactActivity() {
 
   //react-native-screens override
   override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(null);
+    supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
+    super.onCreate(savedInstanceState);
   }
 }
